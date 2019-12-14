fc88cf5785baa3e2681f2a9bfc254fdf64825a06
CHRISTOPHER FORBES <christopherforbes@CHRISTOPHERs-MacBook-Pro.local>
2019-12-14 20:35:42 +0000

    added a bit of loging to actually parse and split the input file string

--- src/OpcodeParser.cc
+++ src/OpcodeParser.cc
@@ -12,27 +12,38 @@ namespace advent
 
     int OpcodeParser::execute_file(std::string input_file_path, int desired_location)
     {
-
+        std::vector<int> program_input = parse_program_file(input_file_path);
         return 0;        
     }
 
     std::vector<int> OpcodeParser::parse_program_file(std::string input_file_path)
     {
-        std::vector<int> parsed_intcodes;
+        std::vector<int> parsed_program;
         std::ifstream input(input_file_path);
         if(input.is_open())
         {
             std::string line;
             while(getline(input, line))
             {
-                std::cout<< "Data line" << line << std::endl;
+                std::cout<< "Splitting Data line: [" << line << "]" << std::endl;
+                std::stringstream string_stream(line);
+                while(string_stream.good())
+                {
+                    std::string temp_;
+                    getline(string_stream,temp_, ',');
+                    parsed_program.push_back(std::stoi(temp_));
+                }
             }
         }
-        return parsed_intcodes;
+        input.close();
+        for(auto &c : parsed_program)
+        {
+            std::cout << c << std::endl;
+        }
+        return parsed_program;
     }
 
     OpcodeParser::~OpcodeParser()
     {
-
     }
-};
+};   
